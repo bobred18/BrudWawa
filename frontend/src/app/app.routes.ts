7e57358d03adabab0963f9a6d4ace9215510b804
Kacper_Cwiek <hunter292@tutanota.com>
2026-06-05 21:33:34 +0200

    added navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,15 @@ import {Login} from './login/login'
 import { Home } from './home/home';
 import { E404 } from './e404/e404';
 import { AddIssue } from './add-issue/add-issue';
+import { Statistics } from './statistics/statistics';
 
 export const routes: Routes = [
-    {path:'', component: Home},
-    {path:'login', component: Login},
-    {path:'home', component: Home},
+    {path:'', component: Home,data: { hideNavbar: true }},
+    {path:'login', component: Login,data: { hideNavbar: true }},
+    {path:'home', component: Home,data: { hideNavbar: true }},
     {path:'add_issue', component: AddIssue},
-    {path:'**', component: E404}
+    {path:'statistics', component: Statistics},
+
+    {path:'**', component: E404,data: { hideNavbar: true }}
 
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { authInterceptor } from './core/auth-interceptor';
 import {GoogleMapsModule} from '@angular/google-maps'
+import { filter } from 'rxjs';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,GoogleMapsModule],
+  imports: [RouterOutlet,GoogleMapsModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,5 +17,23 @@ export class App {
   ngOnInit(){
     sessionStorage.setItem("apiURL","https://brudwawa.duckdns.org/api")
     //https://brudwawa.duckdns.org/api  https://localhost:8000/api
+  }
+  showNavbar = true;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        let route = this.activatedRoute.firstChild;
+
+        while (route?.firstChild) {
+          route = route.firstChild;
+        }
+
+        this.showNavbar = !route?.snapshot.data['hideNavbar'];
+      });
   }
 }
